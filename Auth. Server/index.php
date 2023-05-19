ac801cc9ce5d2926a15387afe5d1ac11bec57780
Pipas <73344068+Piipas@users.noreply.github.com>
2023-05-19 18:14:12 +0100

    Add files via upload

--- Auth. Server/index.php
+++ Auth. Server/index.php
@@ -1,21 +1,9 @@
 <?php
-  require_once "db_connection.php";
-  require_once "helpers/functions.php";
-
   if (isset($_GET["code"])) {
-    $code = preg_match("/[AB0-9]{4}/", $_GET["code"])?$_GET["code"]:NULL;
-    if ($code) {
-      $hashed_code = sha1($code);
-      $is_correct = check_code($hashed_code);
-      if (!$is_correct) {
-        header($_SERVER["SERVER_PROTOCOL"]." 403 Incorrect");
-        die;
-      }
-    } else {
-      header($_SERVER["SERVER_PROTOCOL"]." 403 Incorrect");
-      die;
+    $first_challenges = array("", "", "");
+    $second_challenges = array("", "", "dddddd");
+    $code =  $_GET["code"];
+    if (in_array($code, $first_challenges)) {
+      echo sha1($second_challenges[rand(0, count($second_challenges) - 1)]);
     }
-  } else {
-    header($_SERVER["SERVER_PROTOCOL"]." 403 Incorrect");
-    die;
   }
