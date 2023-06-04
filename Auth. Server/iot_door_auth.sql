778f19882d7d65cbaab528c67b5c177423fa458c
Pipas <ismailpipas@gmail.com>
2023-06-04 23:44:07 +0100

    DB Updated

--- Auth. Server/iot_door_auth.sql
+++ Auth. Server/iot_door_auth.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2023 at 01:54 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Jun 05, 2023 at 12:43 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,17 +30,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `challenges` (
   `challenge` varchar(255) NOT NULL,
   `is_active` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `challenges`
 --
 
 INSERT INTO `challenges` (`challenge`, `is_active`) VALUES
-('07022d0237affe7f3c08809bd91561128112022e', 0),
-('197857152f79919db2442dfa10f0c5ae7759bce5', 0),
-('acc6c3292e292a48cc1996b22bcac891cf9e1794', 0),
-('f4e0dafb5b19bdc74b8ab4a0901bed58e214e50e', 0);
+('wXVp2wMRivi', 0);
 
 -- --------------------------------------------------------
 
@@ -50,14 +47,14 @@ INSERT INTO `challenges` (`challenge`, `is_active`) VALUES
 
 CREATE TABLE `codes` (
   `code` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `codes`
 --
 
 INSERT INTO `codes` (`code`) VALUES
-('0ebcdc7babc0de9a1d6c7d1c180bfcb8183fa492');
+('5678');
 
 --
 -- Indexes for dumped tables
@@ -68,6 +65,12 @@ INSERT INTO `codes` (`code`) VALUES
 --
 ALTER TABLE `challenges`
   ADD UNIQUE KEY `challenge` (`challenge`);
+
+--
+-- Indexes for table `codes`
+--
+ALTER TABLE `codes`
+  ADD PRIMARY KEY (`code`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
